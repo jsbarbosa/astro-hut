e63b51c49d741ec54a2fb755c686ed7c924a9599
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-07-31 18:58:38 -0500

    clean branch

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-from distutils.core import setup, Extension
+from setuptools import setup
+from setuptools.extension import Extension
 
-module1 = Extension('astrohut/bruteforce',
-                    sources = ['astrohut/bruteforce.c', 'astrohut/box.c', 'astrohut/init.c'], include_dirs=['astrohut'], extra_compile_args=["-fopenmp"],
+module = Extension('astrohut/astrohutc',
+                    sources = ['astrohut/core/box2d.c'], include_dirs=['astrohut'], extra_compile_args=["-fopenmp"],
                      extra_link_args=["-fopenmp"])
 
-setup (name = 'AstroHut',
-       version = '1.0',
-       description = 'Bruteforce attemp',
-       packages = ['astrohut'],
-       ext_modules = [module1])
+setup(
+    name = "astrohut",
+    version = "0.0.3",
+    author = "Juan Barbosa",
+    author_email = "js.barbosa10@uniandes.edu.co",
+    description = ('Barnes-Hut NBody simulation library.'),
+    license = "GPL",
+    keywords = "example documentation tutorial",
+    packages=['astrohut'],
+    install_requires=['matplotlib', 'numpy'],
+    ext_modules = [module],
+    long_description="https://github.com/jsbarbosa/astro-hut/",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+    headers = ["astrohut/box2d.h"],
+)
